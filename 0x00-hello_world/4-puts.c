afb680330d06bad3ac53bbc76d76be76913a45d4
Ekefre1 <ekefrembuoty1@gmail.com>
2023-06-17 04:45:29 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
-#!/bin/bash
+# !/ bin / bash
 #include <stdio.h>
 
-int main()
-	{
-    puts("Programming is like building a multilingual puzzle" \n);
-    Return 0;
-	}	
-
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle" \n);
+Return 0;
+}
