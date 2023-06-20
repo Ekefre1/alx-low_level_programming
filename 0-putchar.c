f93fd82d51b61937ad90328b9d4afdedf32504ac
Ekefre1 <ekefrembuoty1@gmail.com>
2023-06-20 11:04:19 -0700

    putchar

---
+++ 0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - command executionexecution 
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-C programming rrpo
+upgrading in programming
